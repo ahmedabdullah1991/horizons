4fe7a006240da2728dc8f0ac3df105e6977ff9fa
ahmed abdullah <ahmedabdullah_088@protonmail.com>
2024-11-19 23:37:51 +0500

    added (files) data.ts, success/route.ts

--- app/api/auth/success/route.ts
+++ app/api/auth/success/route.ts
@@ -1,30 +1,32 @@
+export const dynamic = "force-dynamic";
+
 import {PrismaClient} from "@prisma/client";
-import {user} from "@/lib/kinde-imports";
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import {NextResponse} from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function GET() {
-    const User = await user()
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
 
-    if (!User || !User.id)
+    if (!user || !user.id)
         throw new Error("something went wrong with authentication" + user);
 
     let dbUser = await prisma.user.findUnique({
-        where: {kindeId: User.id}
+        where: {kindeId: user.id}
     });
 
     if (!dbUser) {
         dbUser = await prisma.user.create({
             data: {
-                kindeId: User.id,
-                firstName: User.given_name ?? "",
-                lastName: User.family_name ?? "",
-                email: User.email ?? "" // Using nullish coalescing operator to provide a default empty string value
+                kindeId: user.id,
+                firstName: user.given_name ?? "",
+                lastName: user.family_name ?? "",
+                email: user.email ?? "" // Using nullish coalescing operator to provide a default empty string value
             }
         });
     }
 
-    return NextResponse.redirect("https://horizons-flax.vercel.app/dashboard");
+    return NextResponse.redirect("http://localhost:3000/dashboard");
 }
-
