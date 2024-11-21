5bfeebc46922561a4ba28818293f2fac909a2929
ahmedabdullah1991 <ahmedabdullah_088@protonmail.com>
2024-11-22 01:29:56 +0500

    Update layout.tsx

--- app/(overview)/layout.tsx
+++ app/(overview)/layout.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-//import {redirect} from "next/navigation";
-//import {authentication, user} from "@/lib/kinde-imports";
+import {redirect} from "next/navigation";
+import {authentication, user} from "@/lib/kinde-imports";
 import {NavigationTab} from "@/components/components";
 import {NavigationSheet} from "@/components/client";
 
 export default async function Layout({children}: { children: React.ReactNode }) {
-    //const isUser = await user()
-    //const isAuthenticated = await authentication()
-    //if (isUser && isAuthenticated) {
-    //    redirect("/api/auth/login")
-    //} else {}
+    const isUser = await user()
+    const isAuthenticated = await authentication()
+    if (isUser && isAuthenticated) {
+        redirect("/api/auth/login")
+    } else {
         return (<>
             <NavigationTab
-                children_01={<div className={"flex gap-2.5"}>
+                children_02={<div className={"flex gap-2.5"}>
                     <NavigationSheet/>
                 </div>}
             />
-            {children}</>)
+            {children}</>)}
 }
