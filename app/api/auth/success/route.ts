9b4a86abb5f3a8e775d43ce31fac6013bdbe7971
ahmed abdullah <ahmedabdullah_088@protonmail.com>
2024-11-21 22:37:38 +0500

    line added 'export const dynamic = force dynamic'

--- app/api/auth/success/route.ts
+++ app/api/auth/success/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import {PrismaClient} from "@prisma/client";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import {NextResponse} from "next/server";
