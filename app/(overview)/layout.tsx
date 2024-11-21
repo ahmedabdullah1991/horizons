9ce642d4172d6d3b208a789984848cf5b63987a7
ahmed abdullah <ahmedabdullah_088@protonmail.com>
2024-11-22 01:56:51 +0500

    update layout.tsx and client.tsx

--- app/(overview)/layout.tsx
+++ app/(overview)/layout.tsx
@@ -1,21 +1,16 @@
 import React from "react";
-import {redirect} from "next/navigation";
-import {authentication, user} from "@/lib/kinde-imports";
 import {NavigationTab} from "@/components/components";
 import {NavigationSheet} from "@/components/client";
 
 export default async function Layout({children}: { children: React.ReactNode }) {
-    const isUser = await user()
-    const isAuthenticated = await authentication()
-    if (isUser && isAuthenticated) {
-        redirect("/api/auth/login")
-    } else {
-        return <>
+    return (
+        <>
             <NavigationTab
                 children_01={<div className={"flex gap-2.5"}>
                     <NavigationSheet/>
                 </div>}
             />
-            {children}</>
-    }
+            {children}
+        </>
+    )
 }
--- components/client.tsx
+++ components/client.tsx
@@ -5,9 +5,8 @@ import Link from "next/link"
 import {usePathname} from "next/navigation";
 import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import {clsx} from "clsx";
-import {Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger,} from "@/components/ui/sheet"
+import {Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle} from "@/components/ui/sheet"
 import {Button} from "@/components/ui/button"
-import {Menu} from "lucide-react"
 
 export function NavigationSheet() {
     const pathname = usePathname()
@@ -21,15 +20,6 @@ export function NavigationSheet() {
     if (pathname !== "/") {
         return (
             <Sheet>
-                <SheetTrigger asChild>
-                    <Button
-                        variant="ghost"
-                        className={"h-12 w-12"}
-                    >
-                        <Menu
-                            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-                    </Button>
-                </SheetTrigger>
                 <SheetContent>
                     <SheetHeader>
                         <SheetTitle>Navigation</SheetTitle>
